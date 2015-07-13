a7cdfa64398c7c0618cee28a98847aa881f6fc33
Gianfranco Cecconi <giacecco@dico.im>
2015-07-14 00:23:16 +0100

    Ongoing...

--- pre-dive/process.R
+++ pre-dive/process.R
@@ -2,28 +2,33 @@ require(dplyr)
 
 lookup <- read.csv("CTRY14_RGN14_CTY14_LAD14_WD14_UK_LU.csv", stringsAsFactors = F)
 
-chisoc <- read.csv("Giacecco's Children Society.csv", stringsAsFactors = F)
+chisoc <- read.csv("children_society_16_17_no.csv", stringsAsFactors = F)
+names(chisoc)[1] <- "type"
+names(chisoc)[2] <- "name"
 
-# Drop the columns starting by 'Total' as the information is redundant
-chisoc <- chisoc[, !grepl("^Total", names(chisoc))]
+# Drop the columns without a header and the ones starting by 'Total', as the information is 
+# redundant
+chisoc <- chisoc[, !grepl("^Total", names(chisoc)) & !grepl("^X.", names(chisoc))]
 
 # Fix errors in the original data
 # - mistypes in the names of the regions
-chisoc$X.1 <- ifelse(chisoc$X.1 == "North West Leicerstershire", "North West Leicestershire", chisoc$X.1)
-chisoc$X.1 <- ifelse(chisoc$X.1 == "Vale of the White Horse", "Vale of White Horse", chisoc$X.1)
-chisoc$X.1 <- ifelse(chisoc$X.1 == "Epsom and Erwell", "Epsom and Ewell", chisoc$X.1)
+chisoc$name <- ifelse(chisoc$name == "North West Leicerstershire", "North West Leicestershire", chisoc$name)
+chisoc$name <- ifelse(chisoc$name == "Vale of the White Horse", "Vale of White Horse", chisoc$name)
+chisoc$name <- ifelse(chisoc$name == "Epsom and Erwell", "Epsom and Ewell", chisoc$name)
 # - West Sussex listed as a district rather than a county
-chisoc$X <- ifelse(chisoc$X.1 == "West Sussex CC", "County", chisoc$X)
+chisoc$type <- ifelse(chisoc$name == "West Sussex CC", "County", chisoc$type)
 
 # split the table in districts and counties
-local_authorities <- chisoc[chisoc$X == "District", ]
-counties <- chisoc[chisoc$X == "County", ]
+local_authorities <- chisoc[chisoc$type %in% c("Unitary", "District"), ]
+counties <- chisoc[chisoc$type == "County", ]
 rm(chisoc)
 
 counties <- counties[, 2:ncol(counties)]
 names(counties)[1] <- 'county_name'
 
 local_authorities <- local_authorities[, 2:ncol(local_authorities)]
-names(local_authorities)[1] <- 'la_name'
 
-local_authorities <- left_join(local_authorities, lookup[, c("LAD14CD", "LAD14NM")], by = c("la_name" = "LAD14NM"))
+# associates the code to the districts
+temp <- lookup[, c("LAD14CD", "LAD14NM")]
+temp <- temp[!duplicated(temp), ]
+local_authorities <- left_join(local_authorities, temp, by = c("name" = "LAD14NM"))
