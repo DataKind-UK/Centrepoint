996adb58f0c48038d623ba4525a292089b9c1977
Gianfranco Cecconi <giacecco@dico.im>
2015-07-14 13:08:43 +0100

    Completed processing Children's Society

--- pre-dive/process-childrens-society.R
+++ pre-dive/process-childrens-society.R
@@ -10,15 +10,16 @@ chisoc <- read.csv("children_society_16_17_no.csv", stringsAsFactors = F, na.str
 names(chisoc)[1] <- "type"
 names(chisoc)[2] <- "name"
 
-# Drop the columns without a header and the ones starting by 'Total', as the information is 
-# redundant
-chisoc <- chisoc[, !grepl("^Total", names(chisoc)) & !grepl("^X.", names(chisoc))]
+# Drop the rows without an area name, the columns without a header, and the ones whose name starts 
+# by 'Total', as the information is redundant
+chisoc <- chisoc[!is.na(chisoc$name), !grepl("^Total", names(chisoc)) & !grepl("^X.", names(chisoc))]
+columns_with_statistics <- names(chisoc)[!(names(chisoc) %in% c('type', 'name'))]
 
 # Force everything to _numeric_ after replacing the '<something' values with the smallest integers 
 # not less than the specified
 temp <- sapply(names(chisoc)[!(names(chisoc) %in% c("name", "type"))], function (columnName) { 
     matches <- grepl("^<(\\d+)", chisoc[, columnName])
-    chisoc[, columnName] <<- as.integer(sub("^<(\\d+)", "\\1", chisoc[, columnName], fixed = F))
+    chisoc[, columnName] <<- as.numeric(sub("^<(\\d+)", "\\1", chisoc[, columnName], fixed = F))
     chisoc[matches, columnName] <<- ceiling(chisoc[matches, columnName] / 2)
 })
 
@@ -41,10 +42,16 @@ rm(chisoc)
 counties <- counties[, 2:ncol(counties)]
 local_authorities <- local_authorities[, 2:ncol(local_authorities)]
 
-# associates the code to the districts
+### DISTRICTS
+
+# associates the code to the districts and drop the name
+# note that thanks to the left_join, the counties will be ignored
 temp <- lookup[, c("LAD14CD", "LAD14NM")]
 temp <- temp[!duplicated(temp), ]
 local_authorities <- left_join(local_authorities, temp, by = c("name" = "LAD14NM"))
+local_authorities <- local_authorities[, c("LAD14CD", columns_with_statistics)]
+
+### COUNTIES
 
 # drop the 'CC' postfix from the county names
 counties$name <- sub(" CC$", "", counties$name)
@@ -55,6 +62,7 @@ temp <- temp[!duplicated(temp), ]
 counties <- left_join(counties, temp, by = c("name" = "CTY14NM"))
 
 # associate the Census 2011 code
+# note that thanks to the left_join, the counties will be ignored
 temp <- lookup_2011[, c("LAD11NM", "LAD11CD")]
 temp <- temp[!duplicated(temp), ]
 counties <- left_join(counties, temp, by = c("LAD14NM" = "LAD11NM"))
@@ -63,7 +71,17 @@ counties <- left_join(counties, temp, by = c("LAD14NM" = "LAD11NM"))
 counties <- left_join(counties, reference_geography[, c("la_code", "population")], by = c("LAD11CD" = "la_code"))
 population_by_county <- counties %>% group_by(CTY14CD) %>% summarise(county_population = sum(population)) 
 
-# calculate what part of the county the district is a as a fraction of population
+# calculate what part of the county the local authority is a as a fraction of population
 counties <- left_join(counties, population_by_county, by = c("CTY14CD" = "CTY14CD"))
 counties$perc_of_population <- counties$population / counties$county_population
 counties <- counties[, !names(counties) %in% c("population", "county_population")]
+
+# adapt the statistics to the size of the local authority
+temp <- sapply(names(counties)[!names(counties) %in% c("name", "CTY14CD", "LAD14CD", "LAD14NM", "LAD11CD", "perc_of_population")], function (columnName) { counties[,columnName] <<- ceiling(counties[,columnName] * counties$perc_of_population); })
+
+# prepare to and merge back with local_authorities
+counties <- cbind(LAD14CD = counties$LAD14CD, counties[,columns_with_statistics])
+local_authorities <- rbind(local_authorities, counties)
+
+# and aggregate
+local_authorities <- local_authorities[, c("LAD14CD", columns_with_statistics)] %>% group_by(LAD14CD) %>% summarise_each(funs(sum))
