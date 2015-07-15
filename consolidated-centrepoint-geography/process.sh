163cd7161f7fa32e2255c487c2b86e64a977d983
Gianfranco Cecconi <giacecco@dico.im>
2015-07-15 06:44:35 +0100

    Added reference to 2014 codes in the reference geography

--- consolidated-centrepoint-geography/process.sh
+++ consolidated-centrepoint-geography/process.sh
@@ -85,21 +85,27 @@ psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"CREATE TABLE ni (lgd2014 CHAR(9),
 psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"SELECT AddGeometryColumn('ni', 'geom', 4326, 'MultiPolygon', 2);"
 psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"INSERT INTO ni (lgd2014, lgd2014name, population, geom, area) SELECT 'N92000002' AS lgd2014, 'Northern Ireland' AS lgd2014name, sum(population) AS population, ST_Transform(ST_Multi(ST_SimplifyPreserveTopology(ST_Union(geom), 0.5)), 4326) AS geom, CAST(ROUND(CAST(SUM(hectares) AS NUMERIC), 2) AS NUMERIC) AS area FROM ni_temp_2;"
 
+# Create a lookup table with the latest codes, as some may have changed
+psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"DROP TABLE IF EXISTS uk_2014_lookup;"
+psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"CREATE TABLE uk_2014_lookup (lad14cd CHAR(9), lad14nm VARCHAR);"
+csvfix exclude -f 1,2,5,6,7,8,9,10 "$(dir_resolve data/uk/CTRY14_RGN14_CTY14_LAD14_WD14_UK_LU.csv)" | tail -n +2 | grep -v "^$" | sort | uniq > data/uk/.temp.csv
+psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"COPY uk_2014_lookup (lad14cd, lad14nm) FROM '$(dir_resolve data/uk/.temp.csv)' WITH CSV;"
+
 # Finally, create the UK table including a numeric index, suitable for importing as a QGIS layer, and a spatial index
 psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"DROP TABLE IF EXISTS uk;"
-psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"CREATE TABLE uk (gid SERIAL PRIMARY KEY, la_code CHAR(9), la_name VARCHAR, population INTEGER, area NUMERIC);"
+psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"CREATE TABLE uk (gid SERIAL PRIMARY KEY, la_code CHAR(9), la_name VARCHAR, population INTEGER, area NUMERIC, lad14cd CHAR(9));"
 psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"SELECT AddGeometryColumn('uk', 'geom', 4326, 'MultiPolygon', 2);"
-psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"INSERT INTO uk (la_code, la_name, population, geom, area) SELECT lad11cd, lad11nm, population, geom, area FROM gb;"
-psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"INSERT INTO uk (la_code, la_name, population, geom, area) SELECT lgd2014, lgd2014name, population, geom, area FROM ni;"
+psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"INSERT INTO uk (la_code, la_name, population, geom, area, lad14cd) SELECT a.lad11cd, a.lad11nm, a.population, a.geom, a.area, b.lad14cd FROM gb AS a LEFT JOIN uk_2014_lookup AS b ON a.lad11nm = b.lad14nm;"
+psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"INSERT INTO uk (la_code, la_name, population, geom, area, lad14cd) SELECT a.lgd2014, a.lgd2014name, a.population, a.geom, a.area, b.lad14cd FROM ni AS a LEFT JOIN uk_2014_lookup AS b ON a.lgd2014name = b.lad14nm;"
 psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"CREATE INDEX uk_idx_geom ON uk USING GIST (geom);"
 
 # Clean up the temporary tables and CSV files
-psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"DROP TABLE IF EXISTS gb_boundaries; DROP TABLE IF EXISTS gb_population; DROP TABLE IF EXISTS gb; DROP TABLE IF EXISTS ni_boundaries; DROP TABLE IF EXISTS ni_population; DROP TABLE IF EXISTS ni_sa_la_lookup; DROP TABLE IF EXISTS ni_temp_1; DROP TABLE IF EXISTS ni_temp_2; DROP TABLE IF EXISTS ni;"
-find data -name ".temp.*" -type f -delete
+# psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"DROP TABLE IF EXISTS gb_boundaries; DROP TABLE IF EXISTS gb_population; DROP TABLE IF EXISTS gb; DROP TABLE IF EXISTS ni_boundaries; DROP TABLE IF EXISTS ni_population; DROP TABLE IF EXISTS ni_sa_la_lookup; DROP TABLE IF EXISTS ni_temp_1; DROP TABLE IF EXISTS ni_temp_2; DROP TABLE IF EXISTS ni; DROP TABLE IF EXISTS uk_2014_lookup;"
+# find data -name ".temp.*" -type f -delete
 
 # dump everything to CSV and GeoJSON files
 # Note that:
 # - the use of _to_char_ is required to avoid PostgreSQL using the scientific format for bigger numbers
 rm -rf uk_centrepoint.json uk_centrepoint.csv
-psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"COPY (select la_code, la_name, to_char(population, '9999999') AS population, to_char(area, '999999999D99') AS area FROM uk) TO '$(dir_resolve uk_centrepoint.csv)' WITH CSV HEADER;"
+psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"COPY (select la_code, la_name, to_char(population, '9999999') AS population, to_char(area, '999999999D99') AS area, lad14cd FROM uk) TO '$(dir_resolve uk_centrepoint.csv)' WITH CSV HEADER;"
 ogr2ogr -f GeoJSON uk_centrepoint.json -lco COORDINATE_PRECISION=3 "PG:host=localhost dbname=$DATABASE_NAME" -sql "select * from uk;"
